27a56dec1a98a50eef073e4fb17b3239629523fe
Kshitij Bhoite <kshitijbhoite011@gmail.com>
2024-07-19 21:37:36 +0530

    switching network automaticallly

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(()=>{
     ethereum.on("accountsChanged", (accounts) => {
-      setAddress(accounts[0])
+      setAddress(accounts[0]);
       const getBal = async() => {
         const balance = await ethereum.request({method: "eth_getBalance", params: [accounts[0], 'latest']})
         setBalance(ethers.formatEther(balance));
@@ -17,6 +17,23 @@ function App() {
       getBal();
     })
   },[])
+
+  const changeNetwork = async() => {
+    await ethereum.request(
+      {method: "wallet_addEthereumChain",
+        params: [{
+          chainId: "0x13882",
+          chainName: "Amoy",
+          rpcUrls: ["https://rpc-amoy.polygon.technology/"],
+          nativeCurrency: {
+            name: "MATIC",
+            symbol: "MATIC",
+            decimals: 18,
+          },
+          blockExplorerUrls: ["https://amoy.polygonscan.com/"]
+        }]
+      })
+  }
 
   const requestAccount = async() => {
     // await ethereum.request({method: "wallet_requestPermissions", params:[{eth_accounts:{}}]});
@@ -32,18 +49,25 @@ function App() {
     setBalance(ethers.formatEther(bal));
   }
   return (
-    <>
-      <div className="bg-red-500">
-        <a className="text-3xl text-blue-400 text-center cursor-pointer" onClick={requestAccount}>
+    <div className='items-center flex justify-center flex-col space-y-3 mt-40'>
+
+      {/* connect account */}
+      <div className="bg-red-500 px-5 py-2 text-center cursor-pointer rounded-md">
+        <a className="text-3xl text-blue-400" onClick={requestAccount}>
           {address}
         </a>
       </div>
-      <div className="bg-red-500">
-        <a className="text-3xl text-blue-400 text-center cursor-pointer">
+
+      {/* show account balalce */}
+      <div className="bg-green-400 rounded-md px-5 py-2">
+        <a className="text-3xl text-black text-center cursor-pointer">
           {balance}
         </a>
       </div>
-    </>
+
+      {/* change network button */}
+      <button className='px-5 py-2 bg-gray-400 text-white text-2xl rounded-md' onClick={changeNetwork}>Switch network</button>
+    </div>
   )
 }
 
